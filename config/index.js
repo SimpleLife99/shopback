211c47aa2689bf3cf00a57814f16ab7c929b75a9
suqi <1787689426@qq.com>
2020-07-09 17:03:28 +0800

    suqi

--- config/index.js
+++ config/index.js
@@ -7,15 +7,21 @@ if (process.env.NODE_ENV === 'development') {
         password: 'root',
         database: 'shopadmin',
     }
-}
-if (process.env.NODE_ENV === 'production') {
+} else if (process.env.NODE_ENV === 'production') {
     //生产环境
     mysql_conf = {
         host: 'localhost',
         user: 'root',
-        port:'3306',
+        port: '3306',
         password: 'root',
         database: 'shopadmin'
+    }
+} else {
+    mysql_conf = {
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'shopadmin',
     }
 }
 module.exports = mysql_conf
